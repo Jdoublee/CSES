7b6022cd74d930cb856dc6c73b1771b75d23e9f3
hayjee <hyewon3429@gmail.com>
2020-03-05 18:16:03 +0900

    add comment

--- Introductory Problems/WeirdAlgorithm.cpp
+++ Introductory Problems/WeirdAlgorithm.cpp
@@ -1,3 +1,7 @@
+/*
+ 문제 : Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one.
+ 해결방법 : 문제에 주어진대로 홀짝 경우 나누어 작성. 
+*/
 #include <iostream>
 using namespace std;
 int main() {
